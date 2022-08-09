93ad292015dbf59fec58624a8d2dc266e13736bc
Alejandro <aqealejandro@gmail.com>
2022-08-09 16:00:06 -0700

    optimized

--- FindPrimes/src/javaChallenges/FindPrimes.java
+++ FindPrimes/src/javaChallenges/FindPrimes.java
@@ -7,7 +7,7 @@ public class FindPrimes {
 		int counter = 1; // counts the number of prime numbers found
 		while(counter < 100) { //continues finding prime numbers until 100 found
 			number = number + 2; //check next odd number, starts with 3
-			for(int checker = 2; checker < number + 1; checker++) { //checks if divisible by any number before it
+			for(int checker = 3; checker < number + 1; checker += 2) { //checks if divisible by any number before it
 				if(checker == number) {
 					total += number; //adds prime number
 					counter++;
